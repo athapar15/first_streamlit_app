a8bc53fc8e3a53ed0378048eae9dcc7a7e603a62
Arvind Thapar <athapar@yahoo.com>
2022-05-16 20:07:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,10 +29,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-
-#streamlit.write('The user entered', fruit_choice)
-
-
+streamlit.write('The user entered', fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
 #While we troubleshoot
 streamlit.stop()
@@ -41,10 +40,9 @@ streamlit.stop()
 #streamlit.text(fruityvice_response.json()) #Just writes the data to the screen
 
 #take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it ot the screen as a table
 
-streamlit.dataframe(fruityvice_normalized)
+
 
 #queries account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
